a2b64ebfa6eda0d453c445a15dd555f32c5964f7
Lacir Junior <lacir.junior@outlook.com>
2022-07-22 21:31:17 -0300

    MensalistaService Ajustado para entity

--- EstacionaService/Controllers/MensalistaController.cs
+++ EstacionaService/Controllers/MensalistaController.cs
@@ -8,38 +8,39 @@ using System.Threading.Tasks;
 
 namespace EstacionaService.Controllers
 {
-  public class MensalistaController : Controller
-  {
-    private readonly ILogger<MensalistaController> _logger;
-    private Service.MensalistaService _service;
-
-    public MensalistaController(ILogger<MensalistaController> logger)
+    public class MensalistaController : Controller
     {
-      _logger = logger;
-      _service = new Service.MensalistaService();
+        private readonly ILogger<MensalistaController> _logger;
+        private Service.MensalistaService _service;
+
+        public MensalistaController(ILogger<MensalistaController> logger)
+        {
+            _logger = logger;
+            _service = new Service.MensalistaService();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult ListarMensalistasPF()
+        {
+            var data = _service.ListarMensalistasPF().Select(x => new
+            {
+                cpf = "<p>" + x.Cpf + "</p>",
+                nome = "<p>" + x.Nome + "</p>",
+                placa = "<p>" + x.Placa + "</p>",
+                tipoVeiculo = "<p>" + x.TipoVeiculo + "</p>",
+                descricao = "<p>" + x.Descricao + "</p>",
+                situacao = x.Situacao == false ? "<p>Inativo</p>" : "<p>Ativo</p>",
+                action = $"<a type=\"button\" class=\"btn btn-warning btn-sm\">Teste</a>"
+            }).ToList();
+
+            return StatusCode(200, new { data = data });
+        }
+
+
     }
-
-    public IActionResult Index()
-    {
-      return View();
-    }
-
-    public IActionResult ListarMensalistasPF()
-    {
-      var data = _service.ListarMensalistasPF().Select(x => new
-      {
-        cpf = "<p>" + x.CPF + "</p>",
-        nome = "<p>" + x.Nome + "</p>",
-        placa = "<p>" + x.Placa + "</p>",
-        tipoVeiculo = "<p>" + x.TipoVeiculo + "</p>",
-        descricao = "<p>" + x.Descricao + "</p>",
-        situacao = x.Situacao == false ? "<p>Inativo</p>" : "<p>Ativo</p>",
-        action = $"<a type=\"button\" class=\"btn btn-warning btn-sm\">Teste</a>"
-      }).ToList();
-
-      return StatusCode(200, new { data = data });
-    }
-
-
-  }
 }
--- EstacionaService/Service/MensalistaService.cs
+++ EstacionaService/Service/MensalistaService.cs
@@ -3,32 +3,44 @@ using Microsoft.Extensions.Configuration;
 using System.IO;
 using EstacionaService.Data;
 using System.Collections.Generic;
+using System.Linq;
+using System;
 
 namespace EstacionaService.Service
 {
-  public class MensalistaService
-  {
-    private readonly SqlConnection _sql;
-    public MensalistaService()
+    public class MensalistaService
     {
-      string stringConexao = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetSection("ConnectionString")["StringConexao"];
-      var conexao = File.ReadAllText(stringConexao);
-      _sql = new SqlConnection(conexao);
+        private readonly Ef.EstacionaServiceContext _sql;
+        public MensalistaService()
+        {
+            _sql = new Ef.EstacionaServiceContext();
+        }
+
+
+        public void CadastrarMensalista(Models.MensalistaPFModel mensalista)
+        {
+            _sql.MensalistaPfs.Add(new Ef.MensalistaPf()
+            {
+                Nome = mensalista.Nome,
+                Cpf = mensalista.CPF,
+                DataEntrada = DateTime.Now,
+                Descricao = mensalista.Descricao,
+                Situacao = mensalista.Situacao,
+                Placa = mensalista.Placa,
+                Plano = mensalista.Plano,
+                TipoVeiculo = mensalista.TipoVeiculo,
+                Valor = mensalista.Valor
+            });
+            _sql.SaveChanges();
+        }
+
+        public List<Ef.MensalistaPf> ListarMensalistasPF()
+        {
+           return _sql.MensalistaPfs.Select(x => x).ToList();
+                                     
+            
+
+        }
+
     }
-
-
-    public void CadastrarMensalista(Models.MensalistaPFModel mensalista)
-    {
-      var sql = new ClientesData();
-      sql.CadastrarMensalista(mensalista, _sql);
-    }
-
-    public List<Models.MensalistaPFModel> ListarMensalistasPF()
-    {
-      var sql = new Data.ClientesData();
-
-      return sql.ListarMensalistasPF(_sql);
-    }
-
-  }
 }
